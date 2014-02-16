1d7413f8aae2391ecd668c5af005e12d65e9a3b2
ithinc <tabutils@gmail.com>
2014-02-16 10:48:37 +0800

    "selected" attribute cleanup
    (cherry picked from commit e57e1f015e5a91313331dd7cd3d83508cab9cef9)
    
    Conflicts:
    	chrome/content/tabutils-pt.js
    	chrome/content/tabutils.js
    	chrome/skin/tabutils.css
    	chrome/skin/tabutils_mac.css
    	defaults/preferences/prefs.js

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -38,9 +38,9 @@ pref("extensions.tabutils.highlightRead", false);
 pref("extensions.tabutils.styles.current", '{"bold":true,"color":true,"colorCode":"blue"}');
 pref("extensions.tabutils.styles.unread", '{"italic":true,"color":true,"colorCode":"red"}');
 pref("extensions.tabutils.styles.read", "");
-pref("extensions.tabutils.selector.current", '[selected="true"]');
-pref("extensions.tabutils.selector.unread", '[unread]:not([selected="true"])');
-pref("extensions.tabutils.selector.read", ':not([unread]):not([selected="true"])');
+pref("extensions.tabutils.selector.current", '[selected]');
+pref("extensions.tabutils.selector.unread", '[unread]:not([selected])');
+pref("extensions.tabutils.selector.read", ':not([unread]):not([selected])');
 
 pref("extensions.tabutils.invertAlt", true);
 pref("extensions.tabutils.dragBindingAlive", true);
