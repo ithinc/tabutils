b6280130dfa3a28341b2fabe60fda71cd7679bb1
ithinc <tabutils@gmail.com>
2014-04-05 20:11:58 +0800

    Show "New Tab" menuitem by default

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -104,7 +104,7 @@ pref("extensions.tabutils.hideLoadInBackground", false);
 pref("extensions.tabutils.hideLoadInForeground", true);
 
 pref("extensions.tabutils.menu.menu_tuOptions", true);
-pref("extensions.tabutils.menu.context_newTab", false);
+pref("extensions.tabutils.menu.context_newTab", true);
 pref("extensions.tabutils.menu.context_duplicateTab", false);
 pref("extensions.tabutils.menu.context_reloadEvery", false);
 pref("extensions.tabutils.menu.context_reloadEvery.list", "10,30,60,120,300,600,1800");
