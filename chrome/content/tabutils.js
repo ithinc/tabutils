9dff0271052b9a7b85195f01c0ca3cb475348cc8
YFdyh000 <yfdyh000@gmail.com>
2015-07-18 15:52:23 +0800

    Fix the 'All but New Tab' not recognize "about:newtab"

--- chrome/content/tabutils.js
+++ chrome/content/tabutils.js
@@ -671,7 +671,7 @@ tabutils._tabOpeningOptions = function() {
     if ((function() {
       switch (TU_getPref("extensions.tabutils.openTabNext", 1)) {
         case 1: //All
-        case 2: return aRelatedToCurrent || aReferrerURI || aURI != "about:blank"; //All but New Tab
+        case 2: return aRelatedToCurrent || aReferrerURI || (aURI != "about:blank" && aURI != "about:newtab"); //All but New Tab
         case 3: return aRelatedToCurrent == null ? aReferrerURI : aRelatedToCurrent; //None but Links
         default: return false; //None
       }
